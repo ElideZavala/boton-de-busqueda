daff27937145f9820012c992da1c7262d85214d2
ElideZavala <zavalavinagreelide@gmail.com>
2021-05-30 18:09:54 -0600

    modificaciones

--- .history/script_20210530171734.js
+++
@@ -1,7 +0,0 @@
-const search = document.querySelector('.search');
-const btn = document.querySelector('.btn');
-const input = document.querySelector('.input');
-
-btn.addEventListener('clich', () => {
-	search.classList.add('active')
-})
